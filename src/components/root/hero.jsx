88a0b4cc2989c84405b7bdc9ea5c34bb82b31db2
Glitch-guy0 <prajwalm9845@gmail.com>
2024-12-07 17:36:52 +0530

    video scale animation

--- src/components/root/hero.jsx
+++ src/components/root/hero.jsx
@@ -1,50 +1,102 @@
 "use client";
 
-import { useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useState, useRef } from "react";
+
+gsap.registerPlugin();
 
 export default function Hero() {
+  const totalVideos = 4;
+  const [clicked, setClicked] = useState(false);
   const [currentVideoIndex, setCurrentVideoIndex] = useState(1);
-  {
-    /* 1 => 4 (0 -> 3 + 1) */
-  }
   const [NextVideoIndex, setNextVideoIndex] = useState(2);
   function videoUpdate() {
     setCurrentVideoIndex(NextVideoIndex);
-    setNextVideoIndex(((NextVideoIndex + 1) % 3) + 1);
+    setNextVideoIndex((NextVideoIndex % totalVideos) + 1);
   }
   const videoUrl = (num) => `/videos/hero-${num}.mp4`;
+
+  // elemnts reference
+  const currentVideo = useRef();
+  const nextVideo = useRef();
+
+  useGSAP(
+    () => {
+      // animations
+      // when clicked scale the video
+      if (clicked) {
+        currentVideo.current.pause();
+        gsap.to(nextVideo.current, {
+          duration: 1,
+          opacity: 1,
+          // scale up the whole screen
+          height: "100dvh",
+          width: "100dvw",
+          ease: "power1.inOut",
+          // run this after the animation
+          onComplete: () => {
+            // change to the video
+            videoUpdate();
+            currentVideo.current.play();
+            setClicked(false);
+          },
+        });
+      }
+    },
+    {
+      // dependencies used to trigger the animation
+      dependencies: [clicked],
+      // after animating revert to the original state
+      revertOnUpdate: true,
+    }
+  );
+
   return (
     <main className="min-h-dvh w-screen relative">
       {/* root animation */}
       <video
+        ref={currentVideo}
         src={videoUrl(currentVideoIndex)}
         className="absolute top-0 left-0 object-cover object-center size-full"
         muted
         autoPlay
         loop
       />
-      <div className="group z-10 absolute absolute-center w-1/4 aspect-square min-w-[200px] rounded overflow-hidden">
+      <div
+        ref={nextVideo}
+        className="group z-10 absolute absolute-center w-1/4 aspect-square min-w-[200px] rounded overflow-hidden"
+      >
         {/* mini video animation */}
         <video
           src={videoUrl(NextVideoIndex)}
           muted
-          autoPlay
+          // autoPlay
           loop
-          onClick={videoUpdate}
-          className="absolute absolute-center object-center size-9/12 opacity-0 group-hover:opacity-100 group-hover:size-full object-cover transition-all duration-500 ease-in rounded"
+          onClick={() => {
+            setClicked(true);
+          }}
+          className="absolute  absolute-center object-center size-9/12 opacity-0 group-hover:opacity-100 group-hover:size-full object-cover transition-all duration-500 ease-in rounded"
         />
       </div>
       {/* <div className="relative z-40 h-dvh w-dvw pointer-events-none text-blue-75"> */}
-        {/* this is above all animations */}
-        <div className="absolute z-40 text-white special-font top-5 left-6">
-          {/* top content */}
-          <h1 className="hero-heading uppercase">redefi<b>n</b>e</h1>
-          <p className=" mb-5 font-robert-regular secondary-heading ">Enter the Metagame Layer <br/> Unleash the Play Economy</p>
-        </div>
-        <div className="absolute z-40 text-white special-font bottom-5 right-5">
-          {/* bottom content */}
-          <h1 className="uppercase special-font hero-heading">g<b>a</b>mi<b>n</b>g</h1>
-        </div>
+      {/* this is above all animations */}
+      <div className="absolute z-40 text-white special-font top-5 left-6">
+        {/* top content */}
+        <h1 className="hero-heading uppercase">
+          redefi<b>n</b>e
+        </h1>
+        <p className=" mb-5 font-robert-regular secondary-heading ">
+          Enter the Metagame Layer <br /> Unleash the Play Economy
+        </p>
+        {/* TODO:  have to place a button here (******************) */}
+      </div>
+      <div className="absolute z-40 text-white special-font bottom-5 right-5">
+        {/* bottom content */}
+        <h1 className="uppercase special-font hero-heading">
+          g<b>a</b>mi<b>n</b>g
+        </h1>
+      </div>
       {/* </div> */}
     </main>
   );
