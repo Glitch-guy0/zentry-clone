fe4523de4b7fa8654f31fb770a006fdbb7d8af32
Glitch-guy0 <prajwalm9845@gmail.com>
2024-12-14 12:37:08 +0530

    about image zoomin prototype

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,33 +5,52 @@ import AnimatedText from "./ui/AnimatedText";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import "./svg.css";
 
 export default function About() {
   gsap.registerPlugin(ScrollTrigger);
 
-  const zoomImage = useRef(null);
+  const triggerElement = useRef(null);
+  const imageFrame = useRef(null);
+  const imageBottom = useRef(null);
+
+  // Trigger element
+  const scroller = {
+    trigger: triggerElement.current,
+    start: "top top",
+    end: "+=200%",
+    scrub: 1,
+    markers: true,
+  };
 
   useGSAP(() => {
-    gsap.set(zoomImage.current, {
-      clipPath: "polygon(40% 20%, 60% 20%, 60% 80%, 40% 80%)",
-      scale: 1.5
-    });
-    gsap.to(zoomImage.current,{
-      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+    gsap.to(imageBottom.current, {
       scale: 1,
+      transform: "translateY(0px)",
       scrollTrigger: {
-        trigger: zoomImage.current,
-        start: "center center",
+        trigger: triggerElement.current,
+        start: "top top",
         end: "+=200%",
-        scrub: 0.5,
-        pin: true,
-        pinSpacing: true,
-      },
+        scrub: 1,
+      }
     });
+
+    gsap.to(imageFrame.current, {
+      clipPath: "inset(0% 0% round 0px)",
+      scrollTrigger: {
+        trigger: triggerElement.current,
+        start: "top top",
+        end: "+=200%",
+        scrub: 1,
+        pin: true,
+        pinSpacing: true
+      }
+    })
   });
 
   return (
-    <div className="min-h-screen min-w-screen flex flex-col items-center gap-5">
+    <div className="min-h-screen w-full text-center items-center gap-5">
+      {/******************** working fine ********************** */}
       <h1 className="uppercase font-general text-sm md:text-[10px] font-bold mt-10">
         welcome to Zentry
       </h1>
@@ -39,10 +58,21 @@ export default function About() {
         text="Disc<b>o</b>ver the world's <br/> l<b>a</b>rgest shared adventure"
         className="mt-10"
       />
-      <div className="relative h-svh w-svw origin-top">
-        <div ref={zoomImage} className="w-screen h-screen overflow-hidden">
-          <img src="images/about.webp" alt="" className="w-svw h-svh"/>
+      {/* ******************** end working fine ********************** */}
+
+      <div
+        ref={triggerElement}
+        className="relative h-dvh md:h-dvh w-screen"
+      >
+        <div ref={imageFrame} className="size-full sm:clipPath-lg clipPath-sm bg-blue-500 mx-auto">
+          <img
+            ref={imageBottom}
+            src="images/about.webp"
+            alt=""
+            className="size-full absolute object-cover object-center scale-[1.3] transform translate-y-[90px]"
+          />
         </div>
+        
       </div>
     </div>
   );
